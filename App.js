c1bd744f07f4b3eaa9de03ced5d2ac76b68f08c2
Marlon Almino <alminomarlon@gmail.com>
2023-05-04 18:03:55 -0300

    Componente Post

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { View, Text } from 'react-native'
 
 import Header from './src/components/Header'
+import Post from './src/components/Post'
 
-export default class App extends Component {
-  render () {
-    return (
+export default props => {
+  return (
+    <View style={{ flex: 1 }}>
       <Header />
-    )
-  }
+      <Post image={require('./assets/imgs/fence.jpg')} />
+    </View>
+  )
 }
