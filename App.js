26c7572516e8412d5c43afcec3173537146ae2a0
Marlon Almino <alminomarlon@gmail.com>
2023-05-05 17:59:13 -0300

    Componente Comentários

--- App.js
+++ App.js
@@ -5,10 +5,18 @@ import Header from './src/components/Header'
 import Post from './src/components/Post'
 
 export default props => {
+  const comments = [{
+    nickname: 'Dayane Lima',
+    comment: 'Excelente Foto!'
+  }, {
+    nickname: 'Juan Manuel',
+    comment: 'Tá desfocada! Faço melhor...'
+  }]
+
   return (
     <View style={{ flex: 1 }}>
       <Header />
-      <Post image={require('./assets/imgs/fence.jpg')} />
+      <Post image={require('./assets/imgs/fence.jpg')} comments={comments} />
     </View>
   )
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { StyleSheet, View, Image, Dimensions } from 'react-native'
 
 import Author from './Author'
+import Comments from './Comments'
 
 export default props => {
     return (
         <View style={styles.container}>
             <Image source={props.image} style={styles.image} />
             <Author email='fulano@gmail.com' nickname='Fulano de Tal' />
+            <Comments comments={props.comments} />
         </View>
     )
 }
