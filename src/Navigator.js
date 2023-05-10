ab449ee808f70c8adcd9d5b5743c2549ca8b568a
Marlon Almino <alminomarlon@gmail.com>
2023-05-10 16:15:59 -0300

    Tela Registro

--- src/Navigator.js
+++ src/Navigator.js
@@ -8,9 +8,11 @@ import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
 import Login from './screens/Login'
+import Register from './screens/Register'
 
 const Tab = createBottomTabNavigator()
 const SwitchStack = createStackNavigator()
+const AuthStack = createStackNavigator()
 
 const routeIcon = {
     Feed: 'home',
@@ -20,10 +22,17 @@ const routeIcon = {
 
 export default props => {
 
+    const Auth = () => (
+        <AuthStack.Navigator initialRouteName='Login'>
+            <AuthStack.Screen name='Login' component={Login} />
+            <AuthStack.Screen name='Register' component={Register} />
+        </AuthStack.Navigator>
+    )
+
     const AuthOrProfile = () => (
         <SwitchStack.Navigator screenOptions={{ headerShown: false }}>
             <SwitchStack.Screen name="Home" component={Profile} />
-            <SwitchStack.Screen name="Auth" component={Login} />
+            <SwitchStack.Screen name="Auth" component={Auth} />
         </SwitchStack.Navigator>
     )
 
