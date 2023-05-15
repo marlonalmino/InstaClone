71914b4ef459eb289a4f7ced359c6dc57901d98b
Marlon Almino <alminomarlon@gmail.com>
2023-05-15 16:31:33 -0300

    User no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,30 @@ import {
     Platform,
     Image
 } from 'react-native'
+import { Gravatar } from 'react-native-gravatar'
 
 import icon from '../../assets/imgs/icon.png'
 
+import useUser from '../data/hooks/useUser'
+
 export default props => {
+    const { name, email } = useUser()
+
+    const username = name || 'Anonymous'
+    const gravatar = email 
+        ? <Gravatar options={{ email, secure: true }} 
+            style={styles.avatar} />
+        : null
+
     return (
         <View style={styles.container}>
             <View style={styles.rowContainer}>
                 <Image source={icon} style={styles.image} />
                 <Text style={styles.title}>Insta Clone</Text>
+            </View>
+            <View style={styles.userContainer} >
+                <Text style={styles.user} >{ username }</Text>
+                { gravatar }
             </View>
         </View>
     )
@@ -26,7 +41,9 @@ const styles = StyleSheet.create({
         padding: 10,
         borderBottomWidth: 1,
         borderColor: '#BBB',
-        width: '100%'
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     rowContainer: {
         flexDirection: 'row',
@@ -42,5 +59,18 @@ const styles = StyleSheet.create({
         fontFamily: 'shelter',
         height: 30,
         fontSize: 28
+    },
+    userContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    user: {
+        fontSize: 10,
+        color: '#888'
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        marginLeft: 10
     }
 })
